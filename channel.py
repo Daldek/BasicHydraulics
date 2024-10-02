374d116b2046c1f9581cc787433891a6b0e44baf
Daldek <38719982+Daldek@users.noreply.github.com>
2024-10-02 08:25:23 +0200

    minor style changes

--- channel.py
+++ channel.py
@@ -1,15 +1,16 @@
 import math
 from fractions import Fraction
 
-'''
+"""
 https://en.wikipedia.org/wiki/Manning_formula
-'''
+"""
 
-class Channel():
-    '''Simple class to define channel's XS'''
+
+class Channel:
+    """Simple class to define channel's XS"""
 
     def __init__(self, inclination, roughness_n):
-        '''Basic input parameters'''
+        """Basic input parameters"""
         self.inclination = inclination
         self.roughness_n = roughness_n
         self._hydraulic_radius = None
@@ -17,29 +18,38 @@ class Channel():
         self._flow_rate = None
 
     def hydraulic_radius(self):
-        '''
+        """
         https://en.wikipedia.org/wiki/Manning_formula#Hydraulic_radius
-        '''
+        """
         if self._hydraulic_radius is None:
-            self._hydraulic_radius = self.cross_sectional_area()/self.wetted_perimeter()
+            self._hydraulic_radius = (
+                self.cross_sectional_area() / self.wetted_perimeter()
+            )
+            # print(f"Hydraulic radius is {self._hydraulic_radius:.2f} m")
         return self._hydraulic_radius
 
     def velocity(self):
         if self._velocity is None:
-            self._velocity = (1/self.roughness_n) *\
-            math.pow(self.hydraulic_radius(), Fraction(2, 3)) *\
-            math.pow(self.inclination, 0.5)
+            self._velocity = (
+                (1 / self.roughness_n)
+                * math.pow(self.hydraulic_radius(), Fraction(2, 3))
+                * math.pow(self.inclination, 0.5)
+            )
+            # print(f"Velocity is {self._velocity:.2f} meters per second")
         return self._velocity
 
     def flow_rate(self):
         if self._flow_rate is None:
-            self._flow_rate = self.velocity()*self.cross_sectional_area()
+            self._flow_rate = self.velocity() * self.cross_sectional_area()
+            # print(f"Max flow is: {self._flow_rate:.3f} cubic meters per second")
         return self._flow_rate
 
+
 class Flat(Channel):
-    '''
+    """
     Class for flat surfaces
-    '''
+    """
+
     def __init__(self, depth, width, inclination, roughness_n):
         super().__init__(inclination, roughness_n)
         self.depth = depth
@@ -51,14 +61,15 @@ class Flat(Channel):
         if self._cross_sectional_area is None:
             self._cross_sectional_area = self.depth * self.width
         return self._cross_sectional_area
-    
+
     def wetted_perimeter(self):
         if self._wetted_perimeter is None:
             self._wetted_perimeter = self.width
         return self._wetted_perimeter
 
+
 class TriangularChannel(Channel):
-    '''Class for triangular channels'''
+    """Class for triangular channels"""
 
     def __init__(self, depth, bank_slope, inclination, roughness_n):
         super().__init__(inclination, roughness_n)
@@ -67,33 +78,36 @@ class TriangularChannel(Channel):
         self._cross_sectional_area = None
         self._wetted_perimeter = None
         self._base_width = None
-    
+
     def base_width(self):
-        '''
+        """
         Calculated for half of the cross-section, i.e. a right-angled triangle.
-        '''
+        """
         if self._base_width is None:
             self._base_width = self.depth * self.bank_slope
-        print(f'Base width is {self._base_width} m')
+            print(f"Base width is {self._base_width:.2f} m")
         return self._base_width
-    
+
     def cross_sectional_area(self):
         if self._cross_sectional_area is None:
             self._cross_sectional_area = self.base_width() * self.depth
-        print(f'XS area is {self._cross_sectional_area} sqm')
+            print(f"XS area is {self._cross_sectional_area:.2f} sqm")
         return self._cross_sectional_area
-    
+
     def wetted_perimeter(self):
-        '''
+        """
         https://en.wikipedia.org/wiki/Wetted_perimeter
-        '''
+        """
         if self._wetted_perimeter is None:
-            self._wetted_perimeter = 2 * math.sqrt(math.pow(self.depth, 2) + math.pow(self.base_width(), 2))
-        print(f'Wetter perimeter is {self._wetted_perimeter} m')
+            self._wetted_perimeter = 2 * math.sqrt(
+                math.pow(self.depth, 2) + math.pow(self.base_width(), 2)
+            )
+            print(f"Wetter perimeter is {self._wetted_perimeter:.2f} m")
         return self._wetted_perimeter
 
+
 class RectangularChannel(Channel):
-    '''Class for rectangular channels'''
+    """Class for rectangular channels"""
 
     def __init__(self, depth, width, inclination, roughness_n):
         super().__init__(inclination, roughness_n)
@@ -108,16 +122,16 @@ class RectangularChannel(Channel):
         return self._cross_sectional_area
 
     def wetted_perimeter(self):
-        '''
+        """
         https://en.wikipedia.org/wiki/Wetted_perimeter
-        '''
+        """
         if self._wetted_perimeter is None:
             self._wetted_perimeter = 2 * self.depth + self.width
         return self._wetted_perimeter
 
     def calculate_flow_rates(self):
         flow_rates = {}
-        for h in range(0, int((self.depth*100) + 1), 10):
+        for h in range(0, int((self.depth * 100) + 1), 10):
             self.depth = h / 100.0
             self._cross_sectional_area = None
             self._wetted_perimeter = None
@@ -127,8 +141,9 @@ class RectangularChannel(Channel):
             flow_rates[self.depth] = round(self.flow_rate(), 2)
         return flow_rates
 
+
 class TrapezoidalChannel(Channel):
-    '''Class for trapezoidal channels'''
+    """Class for trapezoidal channels"""
 
     def __init__(self, depth, width, bank_inclination_m, inclination, roughness_n):
         super().__init__(inclination, roughness_n)
@@ -140,21 +155,24 @@ class TrapezoidalChannel(Channel):
 
     def cross_sectional_area(self):
         if self._cross_sectional_area is None:
-            self._cross_sectional_area = ((self.depth*self.bank_inclination_m)+self.width)*self.depth
+            self._cross_sectional_area = (
+                (self.depth * self.bank_inclination_m) + self.width
+            ) * self.depth
         return self._cross_sectional_area
 
     def wetted_perimeter(self):
-        '''
+        """
         https://en.wikipedia.org/wiki/Wetted_perimeter
-        '''
+        """
         if self._wetted_perimeter is None:
-            self._wetted_perimeter = self.width + 2 * self.depth *\
-                  math.sqrt(1 + math.pow(self.bank_inclination_m, 2))
+            self._wetted_perimeter = self.width + 2 * self.depth * math.sqrt(
+                1 + math.pow(self.bank_inclination_m, 2)
+            )
         return self._wetted_perimeter
-    
+
     def calculate_flow_rates(self):
         flow_rates = {}
-        for h in range(0, int((self.depth*100) + 1), 10):
+        for h in range(0, int((self.depth * 100) + 1), 10):
             self.depth = h / 100.0
             self._cross_sectional_area = None
             self._wetted_perimeter = None
@@ -164,8 +182,9 @@ class TrapezoidalChannel(Channel):
             flow_rates[self.depth] = round(self.flow_rate(), 2)
         return flow_rates
 
+
 class SemiCircularChannel(Channel):
-    '''Class for circular channels'''
+    """Class for circular channels"""
 
     def __init__(self, depth, radius, inclination, roughness_n):
         super().__init__(inclination, roughness_n)
@@ -176,30 +195,34 @@ class SemiCircularChannel(Channel):
         self._wetted_perimeter = None
 
     def theta_rad(self):
-        '''
+        """
         https://en.wikipedia.org/wiki/Radian
         https://en.wikipedia.org/wiki/Sine_and_cosine
         The theta angle was calculated as the cosine of the ratio of the height of the right triangle
         (radius of circle - depth of water (sagitta)), i.e. the perpendicular lying at the acute angle,
         to the radius, i.e. the opposite perpendicular.
         The result is then doubled to obtain the angle theta for an isosceles triangle
-        '''
+        """
         if self._theta_rad is None:
-            self._theta_rad = 2 * math.acos((self.radius - self.depth)/self.radius)
+            self._theta_rad = 2 * math.acos((self.radius - self.depth) / self.radius)
         return self._theta_rad
 
     def cross_sectional_area(self):
-        '''
+        """
         https://en.wikipedia.org/wiki/Circular_segment
-        '''
+        """
         if self._cross_sectional_area is None:
-            self._cross_sectional_area = 0.5 * math.pow(self.radius, 2) * (self.theta_rad() - math.sin(self.theta_rad()))
+            self._cross_sectional_area = (
+                0.5
+                * math.pow(self.radius, 2)
+                * (self.theta_rad() - math.sin(self.theta_rad()))
+            )
         return self._cross_sectional_area
 
     def wetted_perimeter(self):
-        '''
+        """
         https://en.wikipedia.org/wiki/Wetted_perimeter
-        '''
+        """
         if self._wetted_perimeter is None:
             self._wetted_perimeter = self.radius * self.theta_rad()
         return self._wetted_perimeter
